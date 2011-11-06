66c5669e3b87c5d1f7443fd4fab86f422f1e40c0
Roberto Valdomiro <roberto.valdomiro@gmail.com>
2011-11-05 22:41:22 -0200

    .

--- src/main/java/gfp/config/GfpHibernateManager.java
+++ src/main/java/gfp/config/GfpHibernateManager.java
@@ -7,7 +7,8 @@ import logus.commons.persistence.hibernate.HibernateDatabase;
 
 public class GfpHibernateManager extends AbstractHibernateManager {
 	
-	private static final String ID = "gfp";
+//	private static final String ID = "gfp";
+	private static final String ID = "gfp-test";
 	
 	public GfpHibernateManager() throws Exception {
 		super(ID, HibernateDatabase.findConfigurationFile(ID), new URI(
