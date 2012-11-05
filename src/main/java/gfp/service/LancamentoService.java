942441191b601930c1ab5ad6db23fdd89c613048
Roberto Valdomiro <roberto.valdomiro@gmail.com>
2012-11-05 09:26:14 -0200

    .

--- src/main/java/gfp/service/LancamentoService.java
+++ src/main/java/gfp/service/LancamentoService.java
@@ -18,6 +18,7 @@ import java.util.Date;
 import java.util.List;
 
 import logus.commons.datetime.DateUtil;
+import logus.commons.log.LogBuilder;
 import logus.commons.number.Number;
 import logus.commons.persistence.hibernate.transaction.HibernateTransaction;
 import logus.commons.persistence.hibernate.transaction.TransactionClass;
@@ -190,6 +191,8 @@ public class LancamentoService extends TransactionClass<LancamentoService> {
 			cdt.add("dataCompensacao between :params2 and :params3");
 		}
 		
+		LogBuilder.info("INICIO::"+dto.getDataInicio());
+		LogBuilder.info("FINAL::"+dto.getDataFinal());
 		prm.add(dto.getDataInicio());
 		prm.add(dto.getDataFinal());
 		
